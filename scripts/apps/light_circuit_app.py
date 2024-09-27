33820f228dbde19a34e4e520fbe0a103dbcf3e97
Kodey Converse <kodey@conve.rs>
2024-09-26 21:35:10 -0400

    Mark like as unavailable when not responding

---
+++ scripts/apps/light_circuit_app.py
@@ -0,0 +1,101 @@
+import json
+import appdaemon.utils
+import typing
+import appdaemon.plugins.hass.hassapi
+import appdaemon.plugins.hass.hassplugin
+
+
+class LightCircuitApp(appdaemon.plugins.hass.hassapi.Hass):
+    _area_id: str
+    _lights: list[dict[str, typing.Any]]
+    _switches: dict[str, typing.Any]
+
+    async def initialize(self) -> None:
+        hass_plugin: appdaemon.plugins.hass.hassplugin.HassPlugin = (
+            await self.AD.plugins.get_plugin_object("hass")
+        )
+        self.ws = await hass_plugin.create_websocket()
+        self._id = 1
+
+        await appdaemon.utils.run_in_executor(
+            self, self.ws.send, json.dumps({"id": self._id, "type": "config/device_registry/list"})
+        )
+        all_devices = json.loads(self.ws.recv())["result"]
+        self._id += 1
+
+        await appdaemon.utils.run_in_executor(
+            self, self.ws.send, json.dumps({"id": self._id, "type": "config/entity_registry/list"})
+        )
+        all_entities = json.loads(self.ws.recv())["result"]
+        self._id += 1
+
+        light_ids = set([light["id"] for light in self.args["lights"]])
+        self._lights = [{**device, "entities": [entity for entity in all_entities if entity["device_id"] == device["id"]]} for device in all_devices if device["id"] in light_ids]  # type: ignore
+
+        self.run_every(self.check_lights, "now", 60)
+
+    async def check_lights(self, cb_args):
+        self.log("checking lights")
+        for light in self._lights:
+            light_entity = [
+                entity
+                for entity in light["entities"]
+                if entity["entity_id"].startswith("light")
+            ][0]
+            state = await self.get_state(
+                entity_id=light_entity["entity_id"], namespace="hass"
+            )
+            if state == "unavailable":
+                continue
+            await appdaemon.utils.run_in_executor(
+                self,
+                self.ws.send,
+                json.dumps(
+                    {
+                        "type": "call_service",
+                        "domain": "light",
+                        "service": f"turn_{state}",
+                        "return_response": False,
+                        "service_data": {
+                            "entity_id": light_entity["entity_id"]
+                        },
+                        "id": self._id,
+                    }
+                ),
+            )
+            light_result = json.loads(self.ws.recv())
+            self._id += 1
+            if not light_result["success"]:
+                self.log(f"Light {light_entity['entity_id']} is unresponsive, marking unavailable")
+                await self.set_state(entity_id=light_entity["entity_id"], state="unavailable", namespace="hass")
+
+
+class Healthcheck:
+    async def initialize(self):
+        # with open(os.path.join(self.app_dir, "lights.yaml")) as file:
+        #     self.config = yaml.safe_load(file)
+
+        # for circuit in self.config["circuits"]:
+        # self.log(circuit["area"])
+
+        # self.adapi = self.get_ad_api()
+
+        # for entity in await self.get_state():
+        #   if "main_bedroom_switch" in str(entity):
+        #     self.log(entity)
+
+        # for entity_id, entity in self.get_state().items():
+        #   if "main_bedroom_ceiling" in entity_id:
+        #     self.log(entity)
+
+        # self.hass = await self.AD.plugins.get_plugin_object("default")
+        # self.log(hass.session.get(""))
+        # ws = await self.hass.create_websocket()
+        # _id = 1
+        # sub = json.dumps({"id": _id, "type": "config/device_registry/list"})
+        # await appdaemon.utils.run_in_executor(self, ws.send, sub)
+        # result = json.loads(ws.recv())
+        # for device in result["result"]:
+        #     if "b4:3a:31:ff:fe:26:f9:96" in str(device):
+        #         self.log(device)
+        pass
